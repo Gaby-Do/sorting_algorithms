622a2ab138591af64c7a57a3a170052f289fcdb1
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-10 08:17:53 -0800

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm.
+ * @array: The array of integers to sort.
+ * @size: Number elements of array.
+ * Return: Sorts new array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+/** lo encararia con un i, j, flag y un temp y supongo que un size -1 seria el delimitador*/
+}
