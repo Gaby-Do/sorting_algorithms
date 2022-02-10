3ddbabc7e91be42a2446d790e99a700c84e534f0
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-10 11:42:18 -0800

    some comentaries

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,4 +12,21 @@
 void bubble_sort(int *array, size_t size)
 {
 /** lo encararia con un i, j, flag y un temp y supongo que un size -1 seria el delimitador*/
+	size_t i;
+	size_t j;
+	size_t temp;
+	
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size, i++)
+	{
+		flag = =0;
+		/*aca creo deberia ir una flag? y un loop para j*/
+
+		if /*condicion if array[j] [j +1] seria?*/
+		{
+			/* aca adentro podria venir un temp = array[j]) y cuando la flag sea 1 hacer un print array?*/
+		}
+
 }
