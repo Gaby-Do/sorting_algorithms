f55df23e800329f309d9831e6608b8b722d2fb6a
Gaby-Do <eme.gaby.do@gmail.com>
2022-02-14 09:20:07 -0800

    bare bones for task 7, version 2

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -9,45 +9,46 @@
 void merge_sort(int *array, size_t size)
 {
 	size_t low = 0, high = size - 1;
+	int *aux;
 
-	m_sort(array, low, high, size);
+	aux = malloc(sizeof(int) * size);
+	if (!aux)
+		return;
+	split(array, low, high, size);
 }
 
-/**
- * split - split an array by the middle
- * @low: first position in array
- * @high: last position in array
- * Return: middle point in the array
- */
-int split(size_t low, size_t high)
-{
-	size_t m;
-
-	m = (high - low) / 2;
-	return (m);
-}
 
 /**
- * m_sort - recursively sorts an array
+ * split - recursively sorts an array
  * @array: array to be sorted
  * @low: position 0 in array
  * @high: final position in array
  * @size: size of original array
  * Return: 0 if ok
  */
-int m_sort(int *array, size_t low, size_t high, size_t size)
+int split(int *array, size_t low, size_t high, size_t size)
 {
 	size_t mid;
 
 	if (low < high)
 	{
-		mid = (high - low) / 2;
+		mid = (low + high) / 2;
 		if (mid - 1 != low)
-			m_sort(array, low, mid - 1, size);
+			split(array, low, mid - 1, size);
 		if (mid != high)
-			m_sort(array, mid, high, size);
+			split(array, mid, high, size);
+		merge(low, mid, high);
 	}
-	return (0);
+	return (*array);
 }
 
+/**
+ * merge - rebuilds the sorted array
+ * @low: position 0 in array
+ * @mid: middle point
+ * @high: final position in array
+ */
+void merge(size_t low, size_t mid, size_t high)
+{
+}
 
