5331f45746c2580b284eb0629e9c865323264671
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-15 05:56:01 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -27,8 +27,11 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-int sort_q(int *array, size_t low, size_t high, size_t size);
+void merge_sort(int *array, size_t size);
 
 /*Other functions*/
+int sort_q(int *array, size_t low, size_t high, size_t size);
+void splitANDmerge(size_t low, size_t high, int *array, int *copia);
+void merging(size_t low, size_t m, size_t high, int *dest, int *origen);
 
 #endif
