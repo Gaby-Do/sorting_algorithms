854e8bc4cefcc857f291ffeb64c2b53ded4036f6
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-02-15 05:54:58 -0800

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,53 +1,81 @@
 #include "sort.h"
 
 /**
+ * merging - merge the values in the position of array and copy
+ * @dest: copy of array, and second array
+ * @origen: source of data
+ * @low: lower bound of array
+ * @m: middle position the final + 1 of the first array and the
+ * first one of the second array
+ * @high: upper bound of array
+ */
+
+void merging(size_t low, size_t m, size_t high, int *dest, int *origen)
+{
+	size_t i = low, j = m, k;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(origen + low, m - low);
+	printf("[right]: ");
+	print_array(origen + m, high - m);
+
+	for (k = low; k < high; k++)
+	{
+		if (i < m && (j >= high || origen[i] <= origen[j]))
+			dest[k] = origen[i], i++;
+		else
+			dest[k] = origen[j], j++;
+	}
+}
+
+
+/**
+* splitANDmerge - Split the given array
+* and merge the sorted sub-arrays.
+* @array: Array of integers to be sorted.
+* @copia: A copy of the array.
+* @low: lower bound of array
+* @high: upper bound of array
+*/
+void splitANDmerge(size_t low, size_t high, int *array, int *copia)
+{
+	size_t m;
+
+	if (high - low < 2)
+		return;
+
+	/*split*/
+	if (high % 2 == 0)
+		m = (low + high) / 2;
+	/*recursion*/
+	splitANDmerge(low, m - 1, array, copia);
+	splitANDmerge(m + 1, high, array, copia);
+	merging(low, m, high, array, copia);
+}
+
+/**
  * merge_sort -  sorts array of ints in ascending order w/ Merge sort algorithm
- *
  * @array: array to be sorted
  * @size: size of array
  */
 void merge_sort(int *array, size_t size)
 {
-	size_t low = 0, high = size - 1;
+	size_t i;
+	int *copia = NULL;
 
-	m_sort(array, low, high, size);
+	if (!array || size < 2)
+		return;
+
+	copia = malloc(sizeof(int) * size);
+
+	if (!copia)
+		return;
+
+	/*copia array*/
+	for (i = 0; i < size; i++)
+		copia[i] = array[i];
+
+	splitANDmerge(0, size, array, copia);
+	free(copia);
 }
-
-/**
- * split - split an array by the middle
- * @low: first position in array
- * @high: last position in array
- * Return: middle point in the array
- */
-int split(size_t low, size_t high)
-{
-	size_t m;
-
-	m = (high - low) / 2;
-	return (m);
-}
-
-/**
- * m_sort - recursively sorts an array
- * @array: array to be sorted
- * @low: position 0 in array
- * @high: final position in array
- * @size: size of original array
- * Return: 0 if ok
- */
-int m_sort(int *array, size_t low, size_t high, size_t size)
-{
-	size_t mid;
-
-	if (low < high)
-	{
-		mid = (high - low) / 2;
-		if (mid - 1 != low)
-			m_sort(array, low, mid - 1, size);
-		if (mid != high)
-			m_sort(array, mid, high, size);
-	}
-	return (0);
-}
-
-
